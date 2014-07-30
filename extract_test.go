d86ddb315d47aa1ca51eae8206a445d13d64d31e
Julian Gruber <julian@juliangruber.com>
2014-07-30 14:16:31 +0200

    add passing test. 79.3% coverage

--- extract_test.go
+++ extract_test.go
@@ -14,6 +14,10 @@ func TestExtract(t *testing.T) {
 	value, err = Extract([]byte("{\"foo\":\"bar\",\"bar\":\"baz\"}"), "foo")
 	check(t, err)
 	assert.Equal(t, value, "bar")
+
+	value, err = Extract([]byte("{\"foo\":\"bar\",\"bar\":\"baz\"}"), "bar")
+	check(t, err)
+	assert.Equal(t, value, "baz")
 }
 
 func check(t *testing.T, err error) {
